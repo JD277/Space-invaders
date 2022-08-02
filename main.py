5a3e6a16df577051a38ced57421ff18e6608141e
Jesus <jdam5000@gmail.com>
2022-08-02 11:22:35 -0400

    adding collisions finished

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from sys import exit
+import math
 import random
+from sys import exit
+
 import pygame
 
 pygame.init()
@@ -15,6 +17,9 @@ bg = pygame.image.load('Graphics/bg.png').convert_alpha()
 # Game active
 game_active = True
 
+# Score
+score = 0
+
 
 # Bullet Sprite
 class Bullet(pygame.sprite.Sprite):
@@ -23,8 +28,8 @@ class Bullet(pygame.sprite.Sprite):
         self.image = pygame.image.load('Graphics/bullet.png').convert_alpha()
         self.rect = self.image.get_rect(
             center=(0, 0))
-        self.image = pygame.transform.rotozoom(self.image, 0, 0.5)
-        self.test = -4
+        self.image = pygame.transform.rotozoom(self.image, 0, 0.6)
+        self.test = -7
         self.rect.x = space_player.sprite.rect.x + 22
         self.rect.y = space_player.sprite.rect.y
 
@@ -34,15 +39,18 @@ class Bullet(pygame.sprite.Sprite):
             self.rect.y += self.test
 
     def destroy(self):
-        if self.rect.y <= 0:
-            self.kill()
+        global state_bullet, score
+        if state_bullet:
+            if self.rect.y <= 20:
+                state_bullet = False
+                self.kill()
 
     def update(self):
         self.movement()
         self.destroy()
 
 
-bullet_sprite = pygame.sprite.Group()
+bullet_sprite = pygame.sprite.GroupSingle()
 state_bullet = False
 
 
@@ -73,39 +81,43 @@ space_player.add(SpacePlayer())
 
 
 # Enemy Sprite
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self, tipo):
-        super().__init__()
-
-        if tipo == 'alien':
-            self.image = pygame.image.load('Graphics/alien.png').convert_alpha()
-            self.rect = self.image.get_rect(center=(random.randint(400, 950), random.randint(300, 500)))
-            self.image = pygame.transform.rotozoom(self.image, 0, 0.15)
-            self.x_pos = 3
-            self.y_pos = -40
-
-    def movements(self):
-        self.rect.x += self.x_pos
-        if self.rect.x >= 720:
-            self.x_pos = -3
-            self.rect.y -= self.y_pos
-        elif self.rect.x <= 0:
-            self.x_pos = 3
-            self.rect.y -= self.y_pos
-
-    def update(self):
-        self.movements()
+enemy_image = pygame.image.load('Graphics/alien.png').convert_alpha()
+enemy_rect = enemy_image.get_rect(center=(random.randint(400, 950), random.randint(300, 500)))
+enemy_image = pygame.transform.rotozoom(enemy_image, 0, 0.15)
+x_pos = 3
+y_pos = -40
 
 
-enemy = pygame.sprite.Group()
-# enemy.add(Enemy('alien'))
+def movements():
+    global x_pos, y_pos
+    enemy_rect.x += x_pos
+    if enemy_rect.x >= 720:
+        x_pos = -3
+        enemy_rect.y -= y_pos
+    elif enemy_rect.x <= 0:
+        x_pos = 3
+        enemy_rect.y -= y_pos
+
+
 enemy_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(enemy_timer, 1000)
 
 
 def collision_of_bullet():
-    if pygame.sprite.groupcollide(bullet_sprite, enemy, True, True):
-        print('')
+    global score, state_bullet
+    if state_bullet:
+        t1 = bullet_sprite.sprite.rect.x
+        t2 = bullet_sprite.sprite.rect.y
+        t3 = enemy_rect.x
+        t4 = enemy_rect.y
+
+        distance = math.sqrt((math.pow(t3-t1, 2)) + (math.pow(t4-t2, 2)))
+        if distance <= 60:
+            enemy_rect.y = random.randint(50, 150)
+            enemy_rect.x = random.randint(100, 700)
+            state_bullet = False
+            bullet_sprite.sprite.kill()
+            score += 5
 
 
 while running:
@@ -115,24 +127,25 @@ while running:
             pygame.quit()
             exit()
 
-        if event.type == enemy_timer:
-            enemy.add(Enemy('alien'))
+        # if event.type == enemy_timer:
+
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and state_bullet is False:
                 bullet_sprite.add(Bullet())
                 state_bullet = True
 
     if game_active:
-        collision_of_bullet()
-        screen.fill('#424b5b')
+
         screen.blit(bg, (0, 0))
+        bullet_sprite.draw(screen)
+        bullet_sprite.update()
         space_player.draw(screen)
         space_player.update()
 
-        enemy.draw(screen)
-        enemy.update()
+        screen.blit(enemy_image, enemy_rect)
+        movements()
 
-        bullet_sprite.draw(screen)
-        bullet_sprite.update()
+        collision_of_bullet()
+
     pygame.display.update()
     clock.tick(60)
